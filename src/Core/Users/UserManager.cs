9747ce8e7c3eee13aa6d693305fa4626d8719e58
Thomas Adams <tjackadams@itadams.co.uk>
2017-03-24 09:21:16 +0000

    logging improvements in the user manager

--- src/Core/Users/UserManager.cs
+++ src/Core/Users/UserManager.cs
@@ -4,18 +4,13 @@
     using System.Collections.Generic;
     using System.DirectoryServices.AccountManagement;
     using System.Linq;
-    using System.Threading.Tasks;
     using Abp.Domain.Services;
-    using Castle.Core.Logging;
     using Common;
-    using Common.Extensions;
     using Models;
     using ShellProgressBar;
 
     public class UserManager : DomainService, IUserManager
     {
-        private readonly object _listOperationLock = new object();
-
         public List<LicenseUser> GetUsersAndGroups(ChildProgressBar childProgressBar)
         {
             childProgressBar?.UpdateMessage("collecting user information from active directory (this process can take some time).");
@@ -28,47 +23,57 @@
         /// <param name="childProgressBar"></param>
         /// <returns></returns>
         private List<LicenseUser> AllUsers(ChildProgressBar childProgressBar)
-        {            
-            using (var context = new PrincipalContext(ContextType.Domain))
+        {
+            try
             {
-                using (var search = new PrincipalSearcher(new UserPrincipal(context)))
-                {                   
-                    var allUsers = search.FindAll().Cast<UserPrincipal>().ToList();
-                    List<LicenseUser> users = new List<LicenseUser>(allUsers.Count);
+                using (PrincipalContext context = new PrincipalContext(ContextType.Domain))
+                {
+                    using (PrincipalSearcher search = new PrincipalSearcher(new UserPrincipal(context)))
+                    {
+                        List<UserPrincipal> allUsers = search.FindAll().Cast<UserPrincipal>().ToList();
+                        List<LicenseUser> users = new List<LicenseUser>(allUsers.Count);
 
-                    using (var pbar = Environment.UserInteractive ? childProgressBar.Spawn(allUsers.Count, "getting users", new ProgressBarOptions
-                    {
-                        ForeGroundColor = ConsoleColor.Yellow,
-                        ProgressCharacter = '─',
-                        BackgroundColor = ConsoleColor.DarkGray,
-                    }) : null)
-                    {
-                        foreach (var user in allUsers)
-                        {
-                            users.Add(new LicenseUser
+                        using (ChildProgressBar pbar = Environment.UserInteractive
+                            ? childProgressBar.Spawn(allUsers.Count, "getting users", new ProgressBarOptions
                             {
-                                DisplayName = user.DisplayName,
-                                Email = user.EmailAddress,
-                                Enabled = user.Enabled ?? false,
-                                FirstName = user.GivenName,
-                                Groups = user.GetAuthorizationGroups().Where(g => g is GroupPrincipal && g.Guid != null).Select(g => new LicenseGroup
+                                ForeGroundColor = ConsoleColor.Yellow,
+                                ProgressCharacter = '─',
+                                BackgroundColor = ConsoleColor.DarkGray
+                            })
+                            : null)
+                        {
+                            foreach (UserPrincipal user in allUsers)
+                            {
+                                users.Add(new LicenseUser
                                 {
-                                    Id = Guid.Parse(g.Guid.ToString()),
-                                    Name = g.Name,
-                                    WhenCreated = DateTime.Parse(g.GetProperty("whenCreated"))
-                                }).ToList(),
-                                Id = Guid.Parse(user.Guid.ToString()),
-                                Surname = user.Surname,
-                                WhenCreated = DateTime.Parse(user.GetProperty("whenCreated"))
-                            });
+                                    DisplayName = user.DisplayName,
+                                    Email = user.EmailAddress,
+                                    Enabled = user.Enabled ?? false,
+                                    FirstName = user.GivenName,
+                                    Groups = user.GetAuthorizationGroups().Where(g => g is GroupPrincipal && g.Guid != null).Select(g => new LicenseGroup
+                                    {
+                                        Id = Guid.Parse(g.Guid.ToString()),
+                                        Name = g.Name,
+                                        WhenCreated = DateTime.Parse(g.GetProperty("whenCreated"))
+                                    }).ToList(),
+                                    Id = Guid.Parse(user.Guid.ToString()),
+                                    Surname = user.Surname,
+                                    WhenCreated = DateTime.Parse(user.GetProperty("whenCreated"))
+                                });
 
-                            pbar?.Tick($"found: {user.DisplayName}");
+                                pbar?.Tick($"found: {user.DisplayName}");
+                            }
                         }
+                        childProgressBar?.Tick();
+                        return users;
                     }
-                    childProgressBar?.Tick();
-                    return users;
                 }
-            }                        
+            }
+            catch (Exception ex)
+            {
+                Logger.Debug(ex.ToString());
+                throw;
+            }
         }
     }
 }
