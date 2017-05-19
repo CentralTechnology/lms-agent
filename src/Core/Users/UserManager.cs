191f13056eeee3e8721d12f76e0311f035cb9e62
Thomas Adams <tjackadams@itadams.co.uk>
2017-05-19 14:03:44 +0100

    refactored the method which retrieves the active directory users.

--- src/Core/Users/UserManager.cs
+++ src/Core/Users/UserManager.cs
@@ -23,58 +23,66 @@
         /// <returns></returns>
         private List<LicenseUser> AllUsers()
         {
+            var localUsers = new List<LicenseUser>();
+
             try
             {
-                var users = new List<LicenseUser>();
-                using (var context = new PrincipalContext(ContextType.Domain))
-                {
-                    using (var search = new PrincipalSearcher(new UserPrincipal(context)))
+                var context = new PrincipalContext(ContextType.Domain);
+
+                // get the groups first
+                var groupSearch = new PrincipalSearcher(new GroupPrincipal(context));
+
+                var groups = groupSearch.FindAll()
+                    .Where(g => g.Guid != null)
+                    .Cast<GroupPrincipal>()
+                    .Where(g => g.IsSecurityGroup != null && (bool) g.IsSecurityGroup)
+                    .Select(g => new
                     {
-                        IEnumerable<UserPrincipal> allUsers = search.FindAll().Cast<UserPrincipal>();
+                        Id = g.Guid,
+                        g.Name,
+                        Members = g.Members.Select(m => m.Guid).ToList(),
+                        WhenCreated = DateTime.Parse(g.GetProperty("whenCreated"))
+                    }).ToList();
 
-                        foreach (UserPrincipal user in allUsers)
-                        {
-                            try
+                // then process the users
+                var userSearch = new PrincipalSearcher(new UserPrincipal(context));
+
+                List<UserPrincipal> users = userSearch.FindAll()
+                    .Where(u => u.Guid != null)
+                    .Cast<UserPrincipal>()
+                    .ToList();
+
+                foreach (UserPrincipal user in users)
+                {
+                    var dirEntry = user.GetUnderlyingObject() as DirectoryEntry;
+
+                    localUsers.Add(new LicenseUser
+                    {
+                        DisplayName = user.DisplayName,
+                        Email = user.EmailAddress,
+                        Enabled = !dirEntry.IsAccountDisabled(),
+                        FirstName = user.GivenName,
+                        Groups = groups.Where(g => g.Members.Any(m => m == user.Guid))
+                            .Select(g => new LicenseGroup
                             {
-                                var dirEntry = user.GetUnderlyingObject() as DirectoryEntry;
-
-                                var localUser = new LicenseUser
-                                {
-                                    DisplayName = user.DisplayName,
-                                    Email = user.EmailAddress,
-                                    Enabled = !dirEntry.IsAccountDisabled(),
-                                    FirstName = user.GivenName,
-                                    Groups = user.GetAuthorizationGroups().Where(g => g is GroupPrincipal && g.Guid != null).Select(g => new LicenseGroup
-                                    {
-                                        Id = Guid.Parse(g.Guid.ToString()),
-                                        Name = g.Name,
-                                        WhenCreated = DateTime.Parse(g.GetProperty("whenCreated"))
-                                    }).ToList(),
-                                    Id = Guid.Parse(user.Guid.ToString()),
-                                    Surname = user.Surname,
-                                    WhenCreated = DateTime.Parse(user.GetProperty("whenCreated"))
-                                };
-
-                                users.Add(localUser);
-                            }
-                            catch (Exception ex)
-                            {
-                                Logger.Error($"There was a problem processing {user.Name}.");
-                                Logger.Error(ex.Message);
-                                Logger.Debug($"Could not convert the following UserPrinciple into a User object: {user.Dump()}");
-                                Logger.Debug(ex.ToString());
-                            }
-                        }
-
-                        return users;
-                    }
+                                Id = Guid.Parse(g.Id.ToString()),
+                                Name = g.Name,
+                                WhenCreated = g.WhenCreated
+                            }).ToList(),
+                        Id = Guid.Parse(user.Guid.ToString()),
+                        Surname = user.Surname,
+                        WhenCreated = DateTime.Parse(user.GetProperty("whenCreated"))
+                    });
                 }
             }
             catch (Exception ex)
             {
+                Logger.Error(ex.Message);
                 Logger.Debug(ex.ToString());
                 throw;
             }
+
+            return localUsers;
         }
     }
 }
