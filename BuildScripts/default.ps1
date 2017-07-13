a15cba92b10993adbfac751615e543a83556654a
Thomas Adams <tjackadams@itadams.co.uk>
2017-07-13 09:12:47 +0100

    update resharper install command

--- BuildScripts/default.ps1
+++ BuildScripts/default.ps1
@@ -255,7 +255,7 @@ Task -Name __InstallReSharperCommandLineTools -Depends __InstallChocolatey -Desc
 
 		if (-not (Test-Path $inspectCodeExe)) {
 			exec {
-				Invoke-Expression "$nugetExe install JetBrains.ReSharper.CommandLineTools -source https://www.nuget.org/api/v2/";
+				Invoke-Expression "$script:chocolateyCommand install resharper-clt -y";
 			}
 		} else {
 			Write-Output "resharper-clt already installed";
