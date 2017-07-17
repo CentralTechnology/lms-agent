e2559c5b1be86299638dc48881c503d93f895135
Thomas Adams <tjackadams@itadams.co.uk>
2017-07-17 09:12:04 +0100

    added the one true error client package. Configured the api credentials. Closes #12

--- src/Service/Program.cs
+++ src/Service/Program.cs
@@ -1,8 +1,10 @@
 ﻿namespace Service
 {
+    using System;
     using Abp;
     using Abp.Timing;
     using Castle.Facilities.Logging;
+    using OneTrueError.Client;
     using Topshelf;
 
     class Runner
@@ -12,6 +14,11 @@
         /// </summary>
         static void Main(string[] args)
         {
+            var insightsUri = new Uri("https://insights.ct.co.uk/");
+            OneTrue.Configuration.Credentials(insightsUri,
+                "a779c7b1c4574095ab4843a96f16d4de",
+                "6411f938a22f4c0c9d297ebbc8d04c7b");
+
             Clock.Provider = ClockProviders.Utc;
 
             HostFactory.Run(sc =>
