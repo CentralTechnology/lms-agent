eb0356674a1266aa3277c5b10f7ca417a89b271f
Thomas Adams <tjackadams@itadams.co.uk>
2017-07-19 10:24:18 +0100

    output tools directory

--- build.ps1
+++ build.ps1
@@ -185,5 +185,6 @@ if (!(Test-Path $CAKE_EXE)) {
 
 # Start Cake
 Write-Host "Running build script..."
+ls tools | Write-Host
 Invoke-Expression "& `"$CAKE_EXE`" `"$Script`" -target=`"$Target`" -configuration=`"$Configuration`" -verbosity=`"$Verbosity`" $UseMono $UseDryRun $UseExperimental $ScriptArgs"
 exit $LASTEXITCODE
