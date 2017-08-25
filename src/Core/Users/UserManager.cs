b065993a8230b786ad9b1b790659719c50ccf918
Thomas Adams <tjackadams@itadams.co.uk>
2017-08-25 15:30:35 +0100

    added a try catch when getting the members from the groups as this can cause an exception if there are invalid entries stored within active directory. closes #34

--- src/Core/Users/UserManager.cs
+++ src/Core/Users/UserManager.cs
@@ -8,8 +8,10 @@
     using Abp.Timing;
     using Common;
     using Common.Extensions;
+    using Dto;
     using Models;
     using NLog;
+    using Abp.Extensions;
 
     public class UserManager
     {
@@ -26,67 +28,115 @@
             try
             {
                 var context = new PrincipalContext(ContextType.Domain);
+                Logger.Debug("Context created.");
 
                 // get the groups first
-                var groupSearch = new PrincipalSearcher(new GroupPrincipal(context));
 
-                var groups = groupSearch.FindAll()
-                    .Cast<GroupPrincipal>()
-                    .Where(g => g.Guid != null)
-                    .Where(g => g.IsSecurityGroup != null && (bool) g.IsSecurityGroup)
-                    .Select(g => new
-                    {
-                        Id = g.Guid,
-                        g.Name,
-                        Members = g.Members.Select(m => m.Guid).ToList(),
-                        WhenCreated = DateTime.Parse(g.GetProperty("whenCreated"))
-                    }).ToList();
-
-                // then process the users
-                var userSearch = new PrincipalSearcher(new UserPrincipal(context));
-
-                List<UserPrincipal> users = userSearch.FindAll()
-                    .Cast<UserPrincipal>()
-                    .Where(u => u.Guid != null)
-                    .ToList();
-
-                foreach (UserPrincipal user in users)
+                using (var groupSearch = new PrincipalSearcher(new GroupPrincipal(context)))
                 {
-                    var dirEntry = user.GetUnderlyingObject() as DirectoryEntry;
+                    Logger.Debug("Searching groups.");
 
-                    try
+                    List<GroupPrincipalOutput> groups = new List<GroupPrincipalOutput>();
+                    foreach (var found in groupSearch.FindAll())
                     {
-                        localUsers.Add(new LicenseUser
+                        try
                         {
-                            DisplayName = user.DisplayName,
-                            Email = user.EmailAddress,
-                            Enabled = !dirEntry.IsAccountDisabled(),
-                            FirstName = user.GivenName,
-                            Groups = groups.Where(g => g.Members.Any(m => m == user.Guid))
-                                .Select(g => new LicenseGroup
-                                {
-                                    Id = Guid.Parse(g.Id.ToString()),
-                                    Name = g.Name,
-                                    WhenCreated = g.WhenCreated
-                                }).ToList(),
-                            Id = Guid.Parse(user.Guid.ToString()),
-                            LastLoginDate = user.LastLogon,
-                            SamAccountName = user.SamAccountName,
-                            Surname = user.Surname,
-                            WhenCreated = DateTime.Parse(user.GetProperty("whenCreated"))
-                        });
+                            var adGroup = found as GroupPrincipal;
+
+                            if (adGroup?.Guid == null)
+                            {
+                                continue;
+                            }
+                            if (adGroup.IsSecurityGroup == null || (bool)!adGroup.IsSecurityGroup)
+                            {
+                                continue;
+                            }
+
+                            List<Guid> members = new List<Guid>();
+                            try
+                            {
+                                members = adGroup.Members.Where(m => m.Guid != null).Select(m => (Guid)m.Guid).ToList();
+                            }
+                            catch (PrincipalOperationException ex)
+                            {
+                                Logger.Error($"Failed to get members from group: {(adGroup.DisplayName.IsNullOrEmpty() ? adGroup.SamAccountName : adGroup.DisplayName)}. " +
+                                    "Most likely cause is that the group contains members which are no longer part of the domain. " +
+                                   "Please investigate the group members in Active Directory.");
+                                Logger.Debug(ex);
+                            }
+
+                            groups.Add(new GroupPrincipalOutput
+                            {
+                                Id = (Guid)adGroup.Guid,
+                                Members = members,
+                                Name = adGroup.Name,
+                                WhenCreated = DateTime.Parse(adGroup.GetProperty("whenCreated"))
+                            });
+                        }
+                        catch (Exception ex)
+                        {
+                            Logger.Error($"Error processing: {found.Guid.Dump()}. Please use the following powershell command to find the group. Get-ADGroup -Identity {found.Guid}");
+                            Logger.Debug(ex);
+                        }
                     }
-                    catch (Exception ex)
+
+                    // then process the users
+                    using (var userSearch = new PrincipalSearcher(new UserPrincipal(context)))
                     {
-                        Logger.Error($"There was an error getting: {user.DisplayName}");
-                        Logger.Debug(ex.ToString());
+                        Logger.Debug("Searching users.");
+
+                        List<UserPrincipal> users = userSearch.FindAll()
+                            .Cast<UserPrincipal>()
+                            .Where(u => u.Guid != null)
+                            .ToList();
+
+                        foreach (UserPrincipal user in users)
+                        {
+                            var dirEntry = user.GetUnderlyingObject() as DirectoryEntry;
+
+                            try
+                            {
+                                localUsers.Add(new LicenseUser
+                                {
+                                    DisplayName = user.DisplayName,
+                                    Email = user.EmailAddress,
+                                    Enabled = !dirEntry.IsAccountDisabled(),
+                                    FirstName = user.GivenName,
+                                    Groups = groups.Where(g => g.Members.Any(m => m == user.Guid))
+                                        .Select(g => new LicenseGroup
+                                        {
+                                            Id = g.Id,
+                                            Name = g.Name,
+                                            WhenCreated = g.WhenCreated
+                                        }).ToList(),
+                                    Id = Guid.Parse(user.Guid.ToString()),
+                                    LastLoginDate = user.LastLogon,
+                                    SamAccountName = user.SamAccountName,
+                                    Surname = user.Surname,
+                                    WhenCreated = DateTime.Parse(user.GetProperty("whenCreated"))
+                                });
+                            }
+                            catch (Exception ex)
+                            {
+                                Logger.Error($"There was an error getting: {user.DisplayName}");
+                                Logger.Debug(ex.ToString());
+                            }
+                        }
                     }
+
+
+
+
                 }
+
+
+
+
             }
             catch (Exception ex)
             {
                 Logger.Error(ex.Message);
-                Logger.Debug(ex.ToString());
+                Logger.Debug(ex.Dump());
                 throw;
             }
 
