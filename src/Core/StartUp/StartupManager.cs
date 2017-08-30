957444446e6cd590fff9ddb29a5ff81712d630eb
Thomas Adams <tjackadams@itadams.co.uk>
2017-08-30 12:19:18 +0100

    separated out the users/veeam startup logic. closes #46

--- src/Core/StartUp/StartupManager.cs
+++ src/Core/StartUp/StartupManager.cs
@@ -48,6 +48,14 @@
                 Logger.Info(monUsers ? "Monitoring Users" : "Not Monitoring Users");
                 Console.WriteLine(Environment.NewLine);
 
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.Message);
+            }
+
+            try
+            {
                 bool monVeeam = MonitorVeeam();
                 SettingManager.ChangeSetting(SettingNames.MonitorVeeam, monVeeam.ToString());
                 Logger.Info(monVeeam ? "Monitoring Veeam" : "Not Monitoring Veeam");
@@ -55,7 +63,6 @@
             }
             catch (Exception ex)
             {
-                RavenClient.Capture(new SentryEvent(ex));
                 Logger.Error(ex.Message);
             }
 
@@ -66,35 +73,43 @@
         private bool MonitorUsers()
         {
             Logger.Info("Dermining whether to monitor users...");
-
-            // check if a domain exists
-            bool domainExists = DirectoryServicesFactory.DirectoryServicesManager().DomainExist();
-            if (!domainExists)
+            try
             {
-                Logger.Warn("Check Domain: FAIL");
-                return false;
-            }
-
-            Logger.Info("Check Domain: OK");
-
-            // check if this is a primary domain controller
-            bool pdc = DirectoryServicesFactory.DirectoryServicesManager().PrimaryDomainController();
-            if (!pdc)
-            {
-                Logger.Warn("Check PDC: FAIL");
-
-                // check override is enabled
-                bool pdcOverride = SettingFactory.SettingsManager().GetSettingValue<bool>(SettingNames.PrimaryDomainControllerOverride);
-                if (!pdcOverride)
+                // check if a domain exists
+                bool domainExists = DirectoryServicesFactory.DirectoryServicesManager().DomainExist();
+                if (!domainExists)
                 {
-                    Logger.Warn("Check PDC Override: FAIL");
+                    Logger.Warn("Check Domain: FAIL");
                     return false;
                 }
 
-                Logger.Warn("Check PDC Override: OK");
-            }
+                Logger.Info("Check Domain: OK");
 
-            Logger.Info("Check PDC: OK");
+                // check if this is a primary domain controller
+                bool pdc = DirectoryServicesFactory.DirectoryServicesManager().PrimaryDomainController();
+                if (!pdc)
+                {
+                    Logger.Warn("Check PDC: FAIL");
+
+                    // check override is enabled
+                    bool pdcOverride = SettingFactory.SettingsManager().GetSettingValue<bool>(SettingNames.PrimaryDomainControllerOverride);
+                    if (!pdcOverride)
+                    {
+                        Logger.Warn("Check PDC Override: FAIL");
+                        return false;
+                    }
+
+                    Logger.Warn("Check PDC Override: OK");
+                }
+
+                Logger.Info("Check PDC: OK");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.Message);
+                Logger.Debug(ex);
+                return false;
+            }
 
             return true;
         }
@@ -102,27 +117,37 @@
         private bool MonitorVeeam()
         {
             Logger.Info("Dermining whether to monitor veeam...");
-            var veeamManager = new VeeamManager();
-
-            // check if veeam is installed
-            bool veeamInstalled = veeamManager.VeeamInstalled();
-            if (!veeamInstalled)
+            try
             {
-                Logger.Warn("Check Veeam Installed: FAIL");
+                var veeamManager = new VeeamManager();
+
+                // check if veeam is installed
+                bool veeamInstalled = veeamManager.VeeamInstalled();
+                if (!veeamInstalled)
+                {
+                    Logger.Warn("Check Veeam Installed: FAIL");
+                    return false;
+                }
+
+                Logger.Info("Check Veeam Installed: OK");
+
+                // check the veeam version
+                string veeamVersion = veeamManager.VeeamVersion();
+                if (veeamVersion == null)
+                {
+                    Logger.Warn("Check Veeam Version: FAIL");
+                    return false;
+                }
+
+                Logger.Info("Check Veeam Version: OK");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.Message);
+                Logger.Debug(ex);
                 return false;
             }
 
-            Logger.Info("Check Veeam Installed: OK");
-
-            // check the veeam version
-            string veeamVersion = veeamManager.VeeamVersion();
-            if (veeamVersion == null)
-            {
-                Logger.Warn("Check Veeam Version: FAIL");
-                return false;
-            }
-
-            Logger.Info("Check Veeam Version: OK");
             return true;
         }
 
