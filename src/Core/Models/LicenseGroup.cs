eec2ca89f16d15cdd51b9ae480caab2ad4f4c4ee
Thomas Adams <tjackadams@itadams.co.uk>
2017-08-31 07:51:59 +0100

    normalize the datetime property. closes #47

--- src/Core/Models/LicenseGroup.cs
+++ src/Core/Models/LicenseGroup.cs
@@ -2,14 +2,20 @@ namespace Core.Models
 {
     using System;
     using System.ComponentModel.DataAnnotations;
-    using Abp.Domain.Entities;
+    using Abp.Timing;
     using Users.Entities;
 
     public class LicenseGroup : LicenseBase
     {
+        private DateTime _whenCreated;
+
         [MaxLength(64)]
         public string Name { get; set; }
 
-        public DateTime WhenCreated { get; set; }
+        public DateTime WhenCreated
+        {
+            get => _whenCreated;
+            set => _whenCreated = Clock.Normalize(value);
+        }
     }
 }
