dcf2128a96c24bfbcff082d0e5d52c2399ea2d56
Thomas Adams <tjackadams@itadams.co.uk>
2017-12-07 11:17:44 +0000

    configured the command line args #83

---
+++ src/Service/ConsoleHost.cs
@@ -0,0 +1,87 @@
+﻿namespace LMS
+{
+    using System;
+    using System.Data.SqlClient;
+    using System.IO;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Sockets;
+    using System.Security;
+    using Abp;
+    using Abp.Dependency;
+    using Abp.Logging;
+    using Castle.Facilities.Logging;
+    using Common.Helpers;
+    using Microsoft.OData.Client;
+    using Startup;
+    using Users;
+    using Veeam;
+
+    public class ConsoleHost
+    {
+        public static void Run(RunOptions opts)
+        {
+            using (AbpBootstrapper bootstrapper = AbpBootstrapper.Create<LMSServiceModule>())
+            {
+                bootstrapper.IocManager
+                    .IocContainer
+                    .AddFacility<LoggingFacility>(f => f.UseLog4Net().WithConfig("log4net.config"));
+
+                bootstrapper.Initialize();
+
+
+                if (opts.Verbose)
+                {
+                    Log4NetHelper.EnableDebug();
+                }
+
+                LogHelper.Logger.Info($"Version: {AppVersionHelper.Version}  Release: {AppVersionHelper.ReleaseDate}");
+
+                if (!opts.SkipStartup)
+                {
+                    using (IDisposableDependencyObjectWrapper<StartupManager> startupManager = bootstrapper.IocManager.ResolveAsDisposable<StartupManager>())
+                    {
+                        bool started = startupManager.Object.Init();
+                        if (!started)
+                        {
+                            return;
+                        }
+                    }
+                }
+
+                try
+                {
+                    if (opts.Monitor == Monitor.Users)
+                    {
+                        using (IDisposableDependencyObjectWrapper<IUserWorkerManager> userWorkerManager = bootstrapper.IocManager.ResolveAsDisposable<IUserWorkerManager>())
+                        {
+                            userWorkerManager.Object.Start();
+                            return;
+                        }
+                    }
+
+                    if (opts.Monitor == Monitor.Veeam)
+                    {
+                        using (IDisposableDependencyObjectWrapper<IVeeamWorkerManager> veeamWorkerManager = bootstrapper.IocManager.ResolveAsDisposable<IVeeamWorkerManager>())
+                        {
+                            veeamWorkerManager.Object.Start();
+                        }
+                    }
+                }
+                catch (Exception ex) when (
+                    ex is DataServiceClientException
+                    || ex is SqlException
+                    || ex is HttpRequestException
+                    || ex is SocketException
+                    || ex is WebException
+                    || ex is SecurityException
+                    || ex is IOException)
+                {
+                    LogHelper.LogException(ex);
+                    LogHelper.Logger.Debug(ex.Message, ex);
+                    LogHelper.Logger.Error("************ Failed ************");
+                }
+            }
+        }
+    }
+}
--- src/Service/Program.cs
+++ src/Service/Program.cs
@@ -1,7 +1,21 @@
 ﻿namespace LMS
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
+    using Abp;
+    using Abp.Configuration;
+    using Abp.Dependency;
+    using Abp.Logging;
     using Abp.Timing;
+    using Castle.Facilities.Logging;
+    using CommandLine;
+    using CommandLine.Text;
     using Core.Common.Constants;
+    using Core.Configuration;
+    using log4net;
+    using log4net.Core;
     using Topshelf;
 
     class Runner
@@ -11,30 +25,199 @@
         /// </summary>
         static void Main(string[] args)
         {
-            Clock.Provider = ClockProviders.Utc;          
-
-            HostFactory.Run(x =>
-            {
-                x.Service<LMSService>(sc =>
+            Parser.Default.ParseArguments<UpdateOptions, RunOptions>(args)
+                .WithParsed<UpdateOptions>(opts =>
                 {
-                    sc.ConstructUsing(() => new LMSService());
+                    using (var bootstrapper = AbpBootstrapper.Create<LMSServiceModule>())
+                    {
+                        bootstrapper.IocManager
+                            .IocContainer
+                            .AddFacility<LoggingFacility>(f => f.UseLog4Net().WithConfig("log4net.config"));
 
-                    sc.WhenStarted(s => s.Start());
-                    sc.WhenStopped(s => s.Stop());
+                        bootstrapper.Initialize();
 
-                    sc.WhenPaused(s => s.Pause());
-                    sc.WhenContinued(s => s.Continue());
+                        using (var settingsManager = bootstrapper.IocManager.ResolveAsDisposable<ISettingManager>())
+                        {
+                            if (opts.AccountId != default(int))
+                            {
+                                settingsManager.Object.ChangeSettingForApplication(AppSettingNames.AutotaskAccountId, opts.AccountId.ToString());
+                                Console.WriteLine($"Account ID: {opts.AccountId}");
+                            }
 
-                    sc.WhenShutdown(s => s.Shutdown());
-                });
+                            if (opts.DeviceId != default(Guid))
+                            {
+                                settingsManager.Object.ChangeSettingForApplication(AppSettingNames.CentrastageDeviceId, opts.DeviceId.ToString());
+                                Console.WriteLine($"Device ID: {opts.DeviceId}");
+                            }
 
-                x.UseLog4Net();
-                x.RunAsLocalSystem();
-                x.SetServiceName(Constants.ServiceName);
-                x.SetDisplayName(Constants.ServiceDisplayName);
-                x.SetDescription(Constants.ServiceDescription);
-                x.StartAutomatically();
-            });
+                            if (opts.PdcOverride.HasValue)
+                            {
+                                settingsManager.Object.ChangeSettingForApplication(AppSettingNames.PrimaryDomainControllerOverride, opts.PdcOverride.Value.ToString());
+                                Console.WriteLine($"PDC Override: {opts.PdcOverride.Value}");
+                            }
+
+                            if (opts.UsersOverride.HasValue)
+                            {
+                                settingsManager.Object.ChangeSettingForApplication(AppSettingNames.UsersOverride, opts.UsersOverride.Value.ToString());
+                                Console.WriteLine($"Users Override: {opts.UsersOverride.Value}");
+                            }
+
+                            if (opts.VeeamOverride.HasValue)
+                            {
+                                settingsManager.Object.ChangeSettingForApplication(AppSettingNames.VeeamOverride, opts.VeeamOverride.Value.ToString());
+                                Console.WriteLine($"Veeam Override: {opts.VeeamOverride.Value}");
+                            }
+                        }
+                    }
+
+                })
+                .WithParsed<RunOptions>(opts =>
+               {
+                   Clock.Provider = ClockProviders.Utc;
+
+                   if (opts.Verbose)
+                   {
+                       ((log4net.Repository.Hierarchy.Hierarchy)LogManager.GetRepository()).Root.Level = Level.Debug;
+                       ((log4net.Repository.Hierarchy.Hierarchy)LogManager.GetRepository()).RaiseConfigurationChanged(EventArgs.Empty);
+                   }
+
+                   bool isService = !Environment.UserInteractive;
+                   if (isService)
+                   {
+                       HostFactory.Run(x =>
+                       {
+                           x.Service<LMSService>(sc =>
+                           {
+                               sc.ConstructUsing(() => new LMSService());
+
+                               sc.WhenStarted(s => s.Start());
+                               sc.WhenStopped(s => s.Stop());
+
+                               sc.WhenPaused(s => s.Pause());
+                               sc.WhenContinued(s => s.Continue());
+
+                               sc.WhenShutdown(s => s.Shutdown());
+                           });
+
+                           x.UseLog4Net();
+                           x.RunAsLocalSystem();
+                           x.SetServiceName(Constants.ServiceName);
+                           x.SetDisplayName(Constants.ServiceDisplayName);
+                           x.SetDescription(Constants.ServiceDescription);
+                           x.StartAutomatically();
+                       });
+                   }
+                   else
+                   {
+                       ConsoleHost.Run(opts);
+                   }
+               });
+
+            if (Debugger.IsAttached)
+            {
+                Console.ReadLine();
+            }
         }
+    }
+
+    [Verb("run", HelpText = "Run a monitor")]
+    public class RunOptions
+    {
+        public RunOptions(Monitor monitor = Monitor.None, bool verbose = false, bool skipStartup = false)
+        {
+            Monitor = monitor;
+            SkipStartup = skipStartup;
+            Verbose = verbose;
+        }
+
+        [Option('m', "monitor", Default = Monitor.None, HelpText = "Runs the requested monitor.")]
+        public Monitor Monitor { get; }
+
+        [Option('s', "skip-startup", Default = false, HelpText = "Avoids running the startup process which checks for api credentials.")]
+        public bool SkipStartup { get; }
+
+        [Option('v', "verbose", Default = false)]
+        public bool Verbose { get; }
+
+        [Usage(ApplicationAlias = "lms.exe")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                yield return new Example("Run \"Users\" monitor", new RunOptions(monitor: Monitor.Users));
+                yield return new Example("Run \"Veeam\" monitor", new RunOptions(monitor: Monitor.Veeam));
+                yield return new Example("Run monitor verbose", new RunOptions(monitor: Monitor.Users, verbose: true));
+                yield return new Example("Run monitor, but skip startup", new RunOptions(monitor: Monitor.Users, skipStartup: true));
+            }
+        }
+    }
+
+    [Verb("update", HelpText = "Update settings")]
+    [SuppressMessage("ReSharper", "ConvertToAutoProperty")]
+    public class UpdateOptions
+    {
+        private readonly bool? _pdcOverride;
+        private readonly bool? _usersOverride;
+        private readonly bool? _veeamOverride;
+        public UpdateOptions()
+        {
+
+        }
+        public UpdateOptions(bool? pdcOverride = null, bool? usersOverride =null, bool? veeamOverride = null)
+        {
+            _pdcOverride = pdcOverride;
+            _usersOverride = usersOverride;
+            _veeamOverride = veeamOverride;
+        }
+        public UpdateOptions(int accountId)
+        {
+            AccountId = accountId;
+        }
+
+        public UpdateOptions(Guid deviceId)
+        {
+            DeviceId = deviceId;
+        }
+
+        public UpdateOptions(int accountId, Guid deviceId)
+        {
+            AccountId = accountId;
+            DeviceId = deviceId;
+        }
+
+        [Option('p', "pdc-override", HelpText = "Allows the users monitor to run from a member server")]
+        public bool? PdcOverride => _pdcOverride;
+
+        [Option("force-users", HelpText = "Enables the user monitoring to run even if the startup checks fail")]
+        public bool? UsersOverride => _usersOverride;
+
+        [Option("force-veeam", HelpText = "Enables the veeam monitoring to run even if the startup checks fail")]
+        public bool? VeeamOverride => _veeamOverride;
+
+        [Option('a', "account", HelpText = "Autotask account id")]
+        public int AccountId { get; set; }
+
+        [Option('d', "device", HelpText = "CentraStage device id")]
+        public Guid DeviceId { get; set; }
+
+        [Usage(ApplicationAlias = "lms.exe")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                yield return new Example("Update Autotask Account", new UpdateOptions(accountId: 12345));
+                yield return new Example("Update CentraStage Device", new UpdateOptions(deviceId: Guid.NewGuid()));
+                yield return new Example("Enable PDC override", new UpdateOptions(pdcOverride: true));
+                yield return new Example("Force user monitoring", new UpdateOptions(usersOverride: true));
+                yield return new Example("Force veeam monitoring", new UpdateOptions(veeamOverride: true));
+            }
+        }
+    }
+
+    public enum Monitor
+    {
+        None = 0,
+        Users = 1,
+        Veeam = 2
     }
 }
