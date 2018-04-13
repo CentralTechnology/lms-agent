8b83cb407a44eef73730e0f4abe411eb1e9b2eda
Thomas Adams <tjackadams@itadams.co.uk>
2018-04-13 10:46:12 +0100

    migrated Assistant project back to msbuild

--- src/LMS.Assistant/Properties/AssemblyInfo.cs
+++ src/LMS.Assistant/Properties/AssemblyInfo.cs
@@ -5,10 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("License Monitoring System Assistant")]
-[assembly: AssemblyDescription("Helper application for the License Monitoring System")]
+[assembly: AssemblyTitle("LMS.Assistant")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Assistant")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LMS.Assistant")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -16,5 +20,17 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d62934d8-8eaa-4ff4-a503-a251743298d2")]
+[assembly: Guid("6c453ad2-df40-458a-ac4b-059b005a4834")]
 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
